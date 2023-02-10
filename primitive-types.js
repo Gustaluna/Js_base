06b04381a5db3cc2fc08d7e310b65d8f870875e4
Gustaluna <lgsantana.cin@gmail.com>
2023-02-09 22:13:30 -0300

    first commit

---
+++ primitive-types.js
@@ -0,0 +1,34 @@
+// Number
+
+const myNumber = 3;
+
+const firstNumber = 1;
+const secondNumber = 2;
+
+
+const mathSum = firstNumber + secondNumber;
+const mathMultiply = firstNumber * secondNumber;
+const mathDivision = firstNumber / secondNumber;
+const mathMinus = firstNumber - secondNumber;
+
+
+
+console.log(mathSum)
+console.log(mathMultiply)
+console.log(mathDivision)
+console.log(mathMinus)
+
+//float
+
+const floatNumber = 3.3
+const floatNumberWhithoutZero = .5
+
+const math = firstNumber / floatNumber 
+
+console.log(math)
+
+// NaN => Not a Number
+
+const text = "math"
+
+console.log (text * firstNumber)
