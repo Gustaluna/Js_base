0fa9073d6c5608bc39f352a8dc00fdef817fbf29
Gustaluna <lgsantana.cin@gmail.com>
2023-02-13 22:10:54 -0300

    conversions

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+const test = true
+const test2 = false
+
+console.log("" == false)
+console.log(0 == false)
+console.log(1 == true)
+
+//null => vazio
+
+let mm;
+let nn = null
+
+console.log(typeof mm) 
+console.log(typeof nn) // => Object
+
+let number = 3
+let string = "Alura"
+
+console.log (typeof number)
+console.log (typeof string)
+
+
+
+//undefined
---
+++ type-conversions.js
@@ -0,0 +1,14 @@
+// Explicit
+const number = 456
+const stringNumber = Number("456")
+
+
+//Number()
+//String()
+console.log(number === stringNumber)
+console.log(number == stringNumber)
+
+console.log(number + Number(stringNumber))
+
+//Implicit
+
