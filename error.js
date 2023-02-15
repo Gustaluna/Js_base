f60cfefeb29f28272b56e16a1d9dbd459652c0c2
Gustaluna <lgsantana.cin@gmail.com>
2023-02-14 23:41:24 -0300

    error

---
+++ error.js
@@ -0,0 +1,13 @@
+//SyntaxError: Missing initializer in const declaration
+const number
+//ReferenceError: letter is not defined
+console.log(letter)
+//SyntaxError: Missing initializer in const declaration
+console.log(number
+//stacktrace
+//at Object.<anonymous> (/home/gustavo/workspace/js-base/error.js:3:13)
+//at Module._compile (node:internal/modules/cjs/loader:1159:14)
+//at Module._extensions..js (node:internal/modules/cjs/loader:1213:10)
+//at Module.load (node:internal/modules/cjs/loader:1037:32)
+//at Module._load (node:internal/modules/cjs/loader:878:12)
+//at Function.executeUserEntryPoint [as runMain
