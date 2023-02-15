8ce438f2d1c6564a6c9c3239ead4b8fb9b0790bc
Gustaluna <lgsantana.cin@gmail.com>
2023-02-15 00:08:21 -0300

    ternary

---
+++ ternary-operator.js
@@ -0,0 +1,10 @@
+const minimumAge = 18
+const clientAge = 16
+
+// if (clientAge >= minimumAge) {
+//     console.log("beer")
+// } else {
+//     console.log("juice")
+// }
+
+console.log(clientAge >= minimumAge ? "beer" : "juice")
