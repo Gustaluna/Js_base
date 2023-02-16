5fc9621acbe98949fcca87b7540ceeaa46c53ac3
Gustaluna <lgsantana.cin@gmail.com>
2023-02-15 21:31:28 -0300

    functions

---
+++ functions.js
@@ -0,0 +1,20 @@
+// let x = ''
+// console.log(x)
+// x = 'Hi'
+// console.log(x)
+
+//Declaration
+function printText (text) {
+    console.log(text)
+}
+
+//Execution
+
+printText(sum())
+
+
+function sum (){
+    return 2 + 2
+}
+
+console.log(sum())
