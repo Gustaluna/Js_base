13c2e7b98807e76ec0810c5c922f3a89ceb6e9c1
Gustaluna <lgsantana.cin@gmail.com>
2023-02-15 22:12:00 -0300

    arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,15 @@
+function introduction(name){
+    return `My name is ${name}`
+}
+
+const introductionArrow = name => `My name is ${name}`
+
+const sum = (n1, n2) => n1 + n2
+
+const sumSmallNumbers = (n1, n2) => {
+    if(n1 > 10 || n2 > 10) {
+        return "Just number between 1 to 9"
+    }else {
+        return num1 + num2
+    }
+}
