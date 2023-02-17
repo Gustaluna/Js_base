d58182237dae22514e8edc44c94e9ab0c8c71b73
Gustaluna <lgsantana.cin@gmail.com>
2023-02-16 21:33:45 -0300

    arrays

---
+++ JS-ARRAY/adding-grade.js
@@ -0,0 +1,7 @@
+const grades = [10, 6, 8]
+
+grades.push(7)
+
+const avarage = (grades [0] + grades [1] + grades [2]) + grades[3]/ avarage.length
+
+console.log(average)
--- JS-ARRAY/avarage.js
+++ JS-ARRAY/avarage.js
@@ -7,6 +7,6 @@
 //              0    1   2   3
 const grades = [10, 6.5, 8, 7.5]
 
-const average = (grades [0] + grades [1] + grades [2] + grades [3]) / 4
+const avarage = (grades [0] + grades [1] + grades [2] + grades [3]) / average.length
 
 console.log(average)
