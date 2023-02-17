a8c53bf6bc396a2e432e3bcd7d18703a5a51f640
Gustaluna <lgsantana.cin@gmail.com>
2023-02-16 21:27:01 -0300

    arrays

---
+++ JS-ARRAY/average.js
@@ -0,0 +1,12 @@
+// const grade1 = 10
+// const grade2 = 6.5
+// const grade3 = 8
+// const grade4 = 7.5
+
+//positions
+//              0    1   2   3
+const grades = [10, 6.5, 8, 7.5]
+
+const average = (grades [0] + grades [1] + grades [2] + grades [3]) / 4
+
+console.log(average)
