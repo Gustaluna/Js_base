7ddde9e833872e2d0b4f98e99e87e90e82ffd4ca
Gustaluna <lgsantana.cin@gmail.com>
2023-02-18 23:55:11 -0300

    list-of-list

---
+++ JS-ARRAY/list-of-list.js
@@ -0,0 +1,6 @@
+const students = ["João", "Juliana", "Ana", "Caio"];
+const avarages = [10, 8, 7.5, 9];
+
+const listOfList =[students,avarages]
+
+console.log(`The second student is: ${listOfList [0] [1]} The grade of ${listOfList [0] [1]} is ${listOfList [1] [1]} `)
