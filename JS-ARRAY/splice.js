a23b9035a9ab62fe2cde21f1b39eefcbe5e5ef97
Gustaluna <lgsantana.cin@gmail.com>
2023-02-18 23:31:25 -0300

    splice

---
+++ JS-ARRAY/splice.js
@@ -0,0 +1,32 @@
+const students = [
+    'João',
+    'Juliana',
+    'Ana',
+    'Caio',
+    'Lara',
+    'Marjorie',
+    'Guilherme',
+    'Aline',
+    'Fabiana',
+    'Andre',
+    'Carlos',
+    'Paulo',
+    'Bia',
+    'Vivian',
+    'Isabela',
+    'Vinícius',
+    'Renan',
+    'Renata',
+    'Daisy',
+    'Camilo'
+]
+
+const class1 = students.slice(0, students.length / 2)
+
+console.log (students)
+
+console.log(class1)
+
+const class2 = students.slice(students.length / 2)
+
+console.log(class2)
