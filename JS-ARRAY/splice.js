fdc1ec0f83ac822b9f81f78fafa0fb296bd548c5
Gustaluna <lgsantana.cin@gmail.com>
2023-02-18 23:46:00 -0300

    aquarium-splice

--- JS-ARRAY/splice.js
+++ JS-ARRAY/splice.js
@@ -3,3 +3,10 @@ const names = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
 names.splice(1, 2, "Rodrigo")
 
 console.log(names)
+
+aquarium = ['🐋', '🐙', '🐬', '🦈']
+
+aquarium.splice(1,0,'🐠')
+aquarium.splice(3,2,'🐟')
+
+console.log(aquarium)
