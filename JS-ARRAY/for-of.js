e6111e384f28c795d17e369d9f4e2afd78f31c53
Gustaluna <lgsantana.cin@gmail.com>
2023-02-21 21:46:46 -0300

    for-of

---
+++ JS-ARRAY/for-of.js
@@ -0,0 +1,13 @@
+const grades = [10, 6.5, 8, 7.5]
+
+let sumOfGrades = 0
+
+for (element of grades) {
+    sumOfGrades += element
+}
+
+console.log(sumOfGrades)
+
+const avarage = sumOfGrades / grades.length
+
+console.log(`The avarage of grades is ${avarage}`)
