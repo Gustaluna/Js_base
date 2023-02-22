b7c171d641c2dca3915f459e924d66b44c7257da
Gustaluna <lgsantana.cin@gmail.com>
2023-02-21 22:01:20 -0300

    callback.js

---
+++ JS-ARRAY/callback.js
@@ -0,0 +1,11 @@
+const names = ["Thamy", "Joel", "Bruna"]
+
+// names.forEach((name) => {
+//     console.log(name)    
+// })
+
+// function printNames(name) {
+//     console.log(name)
+// }
+
+names.forEach(printNames)
