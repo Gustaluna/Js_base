24ac161d98160f3792b678820d3ca54fa7b24d16
Gustaluna <lgsantana.cin@gmail.com>
2023-02-22 23:11:59 -0300

    object

--- OBJECTS/objects.js
+++ OBJECTS/objects.js
@@ -3,3 +3,8 @@ const CPFlist = [1111111,222222,3333333]
 const personInfo = ["name", "Joseph", "age", 32, "CPF", 111222333]
 
 console.log(personInfo[1])
+
+const personObject = {
+    name: "Joseph",
+    age: 32
+}
