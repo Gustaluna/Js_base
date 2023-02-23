8a956b6eeed47e119ffce5fc5de09e3a8010e09c
Gustaluna <lgsantana.cin@gmail.com>
2023-02-23 20:25:30 -0300

    ;

--- OBJECTS/access.js
+++ OBJECTS/access.js
@@ -3,7 +3,7 @@ const client = {
     age: 32,
     cpf: "111222333",
     email: "andre@dominio.com"
-};
+}
 
 console.log(`The name of client is ${client.name}, and the age in years is ${client.age}`)
 
