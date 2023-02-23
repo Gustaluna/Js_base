50717927f7c0047260b7bfc131ea4d244cc71c01
Gustaluna <lgsantana.cin@gmail.com>
2023-02-22 23:24:39 -0300

    access

---
+++ OBJECTS/access.js
@@ -0,0 +1,10 @@
+const client = {
+    name: "Andre",
+    age: 32,
+    cpf: "111222333",
+    email: "andre@dominio.com"
+};
+
+console.log(`The name of client is ${client.name}, and the age in years is ${client.age}`)
+
+console.log(`The 3 first digits of the cpf of this client is ${client.cpf.substring(0, 3)}`);
