fb05ca488e476b7172427def8111877427bcf047
Gustaluna <lgsantana.cin@gmail.com>
2023-02-24 22:08:41 -0300

    ..

--- OBJECTS/access.js
+++ OBJECTS/access.js
@@ -4,7 +4,6 @@ const client = {
     cpf: "111222333",
     email: "andre@dominio.com"
 }
-
 console.log(`The name of client is ${client.name}, and the age in years is ${client.age}`)
 
 console.log(`The 3 first digits of the cpf of this client is ${client.cpf.substring(0, 3)}`);
