243e9a757ef7b766a5bc5bec4bdaa6aaa3112e47
Gustaluna <lgsantana.cin@gmail.com>
2023-02-27 21:57:04 -0300

    manipulation

---
+++ OBJECTS/increase.js
@@ -0,0 +1,16 @@
+const person = {
+    name: "Luma",
+    ocupation: "Engenier",
+};
+
+console.log(person.name);
+
+console.log(person.phone);
+
+person.phone = "11 98329712";
+
+console.log(person.phone);
+
+person.name = "Luma Silva";
+
+console.log(person)
