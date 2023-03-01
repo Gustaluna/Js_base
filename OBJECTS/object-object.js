cb0f8d2b5a811989e88b98af52e348f8ea1342b9
Gustaluna <lgsantana.cin@gmail.com>
2023-02-28 21:08:43 -0300

    object in object

---
+++ OBJECTS/object-object.js
@@ -0,0 +1,15 @@
+const client = {
+    name: "John",    
+    age: 24,
+    email: "john@email.com",
+    phone: ["111333223", "311333223"]
+};
+
+client.adress = {
+    street: "Albert Einstein",
+    number: 123321,
+    apt: true,
+    complement: "apt 934"
+};
+
+console.log(client.adress)
