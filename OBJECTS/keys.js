967928188ba732234b36d331f7d61a76287cf4d9
Gustaluna <lgsantana.cin@gmail.com>
2023-03-03 23:31:06 -0300

    object keys

---
+++ OBJECTS/keys.js
@@ -0,0 +1,22 @@
+const client = {
+    name: "John",    
+    age: 24,
+    email: "john@email.com",
+    phone: ["111333223", "311333223"]
+};
+
+client.adress = [
+    {
+    street: "Albert Einstein",
+    number: 123321,
+    apt: true,
+},
+];
+
+const objectsKeys = Object.keys(client)
+
+console.log(objectsKeys)
+
+if (!objectsKeys.includes("adresss")){
+    console.error("Is necessary to have an address")
+}
